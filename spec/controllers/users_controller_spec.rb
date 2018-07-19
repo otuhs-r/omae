62bd76934755f605c7faabac089fb2991916891a
otuhs_d <yekcim.114@gmail.com>
2018-07-19 22:39:12 +0900

    test: add user controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,38 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
+  describe 'GET #index' do
+    context 'with authentication' do
+      login
+      it 'redirects to root.' do
+        get 'index'
+        expect(response.status).to eq 302
+      end
+
+      it 'returns success status.' do
+        get 'index'
+        expect(response.status).to eq 200
+      end
+
+      it 'displays :index template' do
+        get 'index'
+        expect(response).to render_template :index
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    context 'with authentication' do
+      login
+      it 'returns success status' do
+        get 'show'
+        expect(response.status).to eq 200
+      end
+
+      it 'dispays :show template' do
+        get 'show'
+        expect(response).to render_template :show
+      end
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'devise'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -56,4 +58,8 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
+  config.extend AuthenticationMacros
 end
