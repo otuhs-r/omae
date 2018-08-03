30dde0f64d56fefef222e858d594b332c6f1876b
otuhs_d <yekcim.114@gmail.com>
2018-08-04 08:54:26 +0900

    refactor: simplify view using helper methods

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,13 @@
 module AttendancesHelper
+  def default_clock_in_time(mode, attendance)
+    mode == 'New' ? current_user.work_start_time.to_s(:time) : attendance.clock_in_time.to_s(:time)
+  end
+
+  def default_clock_out_time(mode, attendance)
+    mode == 'New' ? current_user.work_end_time.to_s(:time) : attendance.clock_out_time.to_s(:time)
+  end
+
+  def default_date(mode, attendance)
+    mode == 'New' ? Date.today : attendance.date
+  end
 end
