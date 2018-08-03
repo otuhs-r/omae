5f818caa877db5e0270d39e292caefe9a28013b6
otuhs_d <yekcim.114@gmail.com>
2018-08-03 21:53:59 +0900

    refactor: exec rubocop auto correct

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,5 +60,5 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.extend ControllerMacros, :type => :controller
+  config.extend ControllerMacros, type: :controller
 end
