c69d7775b0df86d2bb42fe28cd517456dbdef083
otuhs_d <yekcim.114@gmail.com>
2018-10-06 12:28:21 +0900

    feat: show days worked on day off

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,11 +1,12 @@
 class Dashboard
-  attr_reader :all_working_seconds, :all_extra_working_seconds,
+  attr_reader :all_working_seconds, :all_extra_working_seconds, :days_worked_off,
               :days_worked, :average_extra_working_seconds, :extra_working_rate
 
   def initialize(user, start_date, end_date)
     @all_working_seconds = user.working_seconds(start_date, end_date)
     @all_extra_working_seconds = user.extra_working_seconds(start_date, end_date)
     @days_worked = user.attendances.where(date: start_date..end_date).count
+    @days_worked_off = user.attendances.where(date: start_date..end_date, division: :off_day).count
     @average_extra_working_seconds = days_worked.zero? ? 0 : @all_extra_working_seconds / @days_worked
     @extra_working_rate = @all_working_seconds.zero? ? 0 : ((@all_extra_working_seconds / @all_working_seconds) * 100).to_i
   end
