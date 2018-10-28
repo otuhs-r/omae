59c973cf54db3f615ec8228031aeccf9cc6a4c95
otuhs_d <yekcim.114@gmail.com>
2018-10-28 20:41:50 +0900

    refactor: show percentage of working hours

--- app/controllers/api/attendances_controller.rb
+++ app/controllers/api/attendances_controller.rb
@@ -3,6 +3,7 @@ class Api::AttendancesController < Api::BaseController
     attendances = current_user.attendances.where(date: @start_date..@end_date)
     @dashboard = Dashboard.new(attendances)
     @days_all = (@end_date - @start_date).to_i + 1
+    @all_seconds = @end_date.to_time - @start_date.to_time + 60
     respond_to do |format|
       format.js
     end
