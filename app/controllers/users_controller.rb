33d22866c19621517cc865e383159127836bc48b
otuhs_d <yekcim.114@gmail.com>
2018-11-25 13:05:32 +0900

    fix: remove email column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user_name = current_user.user_name
-    @email = current_user.email
     @work_start_time = current_user.work_start_time
     @work_end_time = current_user.work_end_time
     @rest_start_time = current_user.rest_start_time
