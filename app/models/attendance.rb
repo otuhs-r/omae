7db9547fc6376c68b1276e878a19b01e21e3b36f
otuhs_d <yekcim.114@gmail.com>
2019-02-07 23:54:46 +0900

    refactor: attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -20,68 +20,99 @@ class Attendance < ApplicationRecord
   end
 
   def time_validation
-    if clock_in_time.present? && clock_out_time.present?
-      if clock_in_time.to_s(:time) >= clock_out_time.to_s(:time)
-        errors.add(:clock_out_time, 'should be later than clock in time.')
-      end
+    return if clock_in_time.blank? || clock_out_time.blank?
+
+    if clock_in_time >= clock_out_time
+      errors.add(:clock_out_time, 'は出勤時刻より後にしてください')
     end
+  end
+
+  def clock_in_time
+    self[:clock_in_time].present? ? Time.parse(self[:clock_in_time].to_s(:time)) : nil
+  end
+
+  def clock_out_time
+    self[:clock_out_time].present? ? Time.parse(self[:clock_out_time].to_s(:time)) : nil
+  end
+
+  def work_start_time
+    Time.parse(user.work_start_time.to_s(:time))
+  end
+
+  def work_end_time
+    Time.parse(user.work_end_time.to_s(:time))
+  end
+
+  def rest_start_time
+    Time.parse(user.rest_start_time.to_s(:time))
+  end
+
+  def rest_end_time
+    Time.parse(user.rest_end_time.to_s(:time))
   end
 
   private
 
   def diff_sec_between_clock_in_out
-    setup_times
-    if @clock_in_time.between?(@rest_start_time, @rest_end_time) &&
-       @clock_out_time.between?(@rest_start_time, @rest_end_time)
-      0.0
-    else
-      @clock_out_time - @clock_in_time
-    end
+    clock_out_time - clock_in_time
   end
 
   def resting_sec
-    setup_times
-    return 0.0 unless @rest_start_time.between?(@clock_in_time, @clock_out_time) ||
-                      @rest_end_time.between?(@clock_in_time, @clock_out_time)
-    if @clock_in_time <= @rest_start_time
-      if @rest_end_time <= @clock_out_time
-        @rest_end_time - @rest_start_time
-      elsif @rest_start_time <= @clock_out_time
-        @clock_out_time - @rest_start_time
-      end
-    elsif @clock_in_time <= @rest_end_time
-      if @rest_end_time <= @clock_out_time
-        @rest_end_time - @clock_in_time
-      end
+    case
+    when clock_in_before_rest_start? && clock_out_after_rest_end?
+      rest_end_time - rest_start_time
+    when clock_in_before_rest_start? && clock_out_during_recess?
+      clock_out_time - rest_start_time
+    when clock_in_before_rest_start? && clock_out_before_rest_start?
+      0.0
+    when clock_in_during_recess? && clock_out_after_rest_end?
+      rest_end_time - clock_in_time
+    when clock_in_during_recess? && clock_out_during_recess?
+      clock_out_time - clock_in_time
+    else
+      0.0
     end
   end
 
+  def clock_in_before_rest_start?
+    clock_in_time <= rest_start_time
+  end
+
+  def clock_in_during_recess?
+    clock_in_time.between?(rest_start_time, rest_end_time)
+  end
+
+  def clock_in_after_rest_end?
+    rest_end_time <= clock_in_time
+  end
+
+  def clock_out_before_rest_start?
+    clock_out_time <= rest_start_time
+  end
+
+  def clock_out_during_recess?
+    clock_out_time.between?(rest_start_time, rest_end_time)
+  end
+
+  def clock_out_after_rest_end?
+    rest_end_time <= clock_out_time
+  end
+
   def earlier_extra_sec
-    setup_times
-    if @work_start_time <= @clock_out_time
-      earlier_sec = @work_start_time - @clock_in_time
+    if work_start_time <= clock_out_time
+      earlier_sec = work_start_time - clock_in_time
       earlier_sec >= 0 ? earlier_sec : 0.0
     else
-      @clock_out_time - @clock_in_time
+      clock_out_time - clock_in_time
     end
   end
 
   def later_extra_sec
-    setup_times
-    if @clock_in_time <= @work_end_time
-      later_sec = @clock_out_time - @work_end_time
+    if clock_in_time <= work_end_time
+      later_sec = clock_out_time - work_end_time
       later_sec >= 0 ? later_sec : 0.0
     else
-      @clock_out_time - @clock_in_time
+      clock_out_time - clock_in_time
     end
-  end
-
-  def setup_times
-    @clock_in_time = Time.parse(clock_in_time.to_s(:time))
-    @clock_out_time = Time.parse(clock_out_time.to_s(:time))
-    @work_start_time = Time.parse(user.work_start_time.to_s(:time))
-    @work_end_time = Time.parse(user.work_end_time.to_s(:time))
-    @rest_start_time = Time.parse(user.rest_start_time.to_s(:time))
-    @rest_end_time = Time.parse(user.rest_end_time.to_s(:time))
   end
 end
