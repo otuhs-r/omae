23d83cc89d6f95e1a0ec31541af998c895470ffe
otuhs_d <yekcim.114@gmail.com>
2019-02-11 14:19:33 +0900

    refactor: refactor labor standard low board

--- spec/models/labor_standard_law_board_spec.rb
+++ spec/models/labor_standard_law_board_spec.rb
@@ -4,51 +4,93 @@ describe LaborStandardLawBoard do
   include ApplicationHelper
 
   describe 'labor standard law' do
-    before(:each) do
-      user = build(:user)
-      3.times { create(:attendance_apr, user: user) }
-      5.times { create(:attendance_may, user: user) }
-      5.times { create(:attendance_jun, user: user) }
-    end
+    context 'attendances exist' do
+      before(:each) do
+        user = build(:user)
+        3.times { create(:attendance_apr, user: user) }
+        5.times { create(:attendance_may, user: user) }
+        10.times { create(:attendance_jun, user: user) }
+      end
 
-    after(:each) do
-      DatabaseCleaner.clean
-    end
+      after(:each) do
+        DatabaseCleaner.clean
+        FactoryBot.reload
+      end
 
-    let(:lsl_board) { LaborStandardLawBoard.new }
+      let(:lsl_board) { LaborStandardLawBoard.new }
 
-    it 'working_hours_group_by_month' do
-      travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
-        expect(lsl_board.working_hours_group_by_month).to eq(
-          [['07月', 0.00], ['08', 0.00], ['09月', 0.00], ['10月', 0.00], ['11月', 0.00], ['12月', 0.00],
-           ['01月', 0.00], ['02月', 0.00], ['03月', 0.00], ['04月', 54.00], ['05月', 90.00], ['06月', 180.00]]
-        )
+      it 'extra_working_hours_group_by_month' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(lsl_board.extra_working_hours_group_by_month).to eq(
+            [['07月', 0.00], ['08月', 0.00], ['09月', 0.00], ['10月', 0.00], ['11月', 0.00], ['12月', 0.00],
+             ['01月', 0.00], ['02月', 0.00], ['03月', 0.00], ['04月', 30.00], ['05月', 50.00], ['06月', 100.00]]
+          )
+        end
+      end
+
+      it 'extra_working_seconds_of_this_month' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(convert_to_hh_mm_from_sec(lsl_board.extra_working_seconds_of_this_month)).to eq '100:00'
+        end
+      end
+
+      it 'extra_working_seconds_of_this_year' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(convert_to_hh_mm_from_sec(lsl_board.extra_working_seconds_of_this_year)).to eq '180:00'
+        end
+      end
+
+      it 'count_more_than_45hours' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(lsl_board.count_more_than_45hours).to eq 2
+        end
+      end
+
+      it 'averages_of_extra_working_seconds' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(lsl_board.averages_of_extra_working_seconds).to eq(
+            [['2ヶ月', 270000.0], ['3ヶ月', 216000.0], ['4ヶ月', 162000.0], ['5ヶ月', 129600.0], ['6ヶ月', 108000.0]]
+          )
+        end
       end
     end
 
-    it 'working_seconds_of_this_month' do
-      travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
-        expect(convert_to_hh_mm_from_sec(lsl_board.working_seconds_of_this_month)).to eq '180:00'
-      end
-    end
+    context 'attendances do not exist' do
+      let(:lsl_board) { LaborStandardLawBoard.new }
 
-    it 'working_seconds_of_this_year' do
-      travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
-        expect(convert_to_hh_mm_from_sec(lsl_board.working_seconds_of_this_year)).to eq '324:00'
+      it 'extra_working_hours_group_by_month' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(lsl_board.extra_working_hours_group_by_month).to eq(
+            [['07月', 0.00], ['08月', 0.00], ['09月', 0.00], ['10月', 0.00], ['11月', 0.00], ['12月', 0.00],
+             ['01月', 0.00], ['02月', 0.00], ['03月', 0.00], ['04月', 0.00], ['05月', 0.00], ['06月', 0.00]]
+          )
+        end
       end
-    end
 
-    it 'count_more_than_45hours' do
-      travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
-        expect(lsl_board.count_more_than_45hours).to eq 2
+      it 'extra_working_seconds_of_this_month' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(convert_to_hh_mm_from_sec(lsl_board.extra_working_seconds_of_this_month)).to eq '00:00'
+        end
       end
-    end
 
-    it 'averages_of_extra_working_seconds' do
-      travel_to(Time.zone.local(2018, 9, 30, 23, 0, 0)) do
-        expect(lsl_board.averages_of_extra_working_seconds).to eq(
-          [['2ヶ月': 270000.0], ['3ヶ月': 216000.0], ['4ヶ月': 162000.0], ['5ヶ月': 129600.0], ['6ヶ月': 108000.0]]
-        )
+      it 'extra_working_seconds_of_this_year' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(convert_to_hh_mm_from_sec(lsl_board.extra_working_seconds_of_this_year)).to eq '00:00'
+        end
+      end
+
+      it 'count_more_than_45hours' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(lsl_board.count_more_than_45hours).to eq 0
+        end
+      end
+
+      it 'averages_of_extra_working_seconds' do
+        travel_to(Time.zone.local(2018, 6, 30, 23, 0, 0)) do
+          expect(lsl_board.averages_of_extra_working_seconds).to eq(
+            [['2ヶ月', 0.0], ['3ヶ月', 0.0], ['4ヶ月', 0.0], ['5ヶ月', 0.0], ['6ヶ月', 0.0]]
+          )
+        end
       end
     end
   end
