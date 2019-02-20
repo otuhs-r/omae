396e3f6fa3bd2e1e731a892e99b916fb909adab3
otuhs_d <yekcim.114@gmail.com>
2019-02-20 21:03:55 +0900

    refactor: add complex index to attendance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_115105) do
+ActiveRecord::Schema.define(version: 2019_02_20_120206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_115105) do
     t.date 'date'
     t.integer 'division', default: 0, null: false
     t.index ['date'], name: 'index_attendances_on_date'
+    t.index %w[user_id date], name: 'index_attendances_on_user_id_and_date'
     t.index ['user_id'], name: 'index_attendances_on_user_id'
   end
 
